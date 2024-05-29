6c37a479b79e887a7008d8a74a4d095b79b56eb7
Renato Santos <renato_jfs@hotmail.com>
2024-05-28 23:27:51 -0300

    Criando um servidor local de desenvolvimento

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <h1 id='title'>
-    <span>Hello JS World!</span>
+    <span>Hello JS World!!</span>
   </h1>,
   document.getElementById('root'),
 );
