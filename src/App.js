9921e51a613fa556c0d14fd69392ca1d3cbc47d1
Renato Santos <renato_jfs@hotmail.com>
2024-06-11 21:32:41 -0300

    Fundamentos do React Expressões JS no JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const category = 'Posts da semana';
+
 function App() {
   return (
     <>
-      <h1>Component App</h1>
-      <h2>Subtítulo</h2>
+      <h1>JStck's Blog {Math.random()}</h1>
+      <h2>{category}</h2>
     </>
   );
 }
