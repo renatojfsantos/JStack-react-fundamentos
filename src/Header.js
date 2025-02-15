a2c85f344f9ce82dd0924ba49bfa34894a4cc88c
Renato Santos <renato_jfs@hotmail.com>
2025-02-15 01:51:41 -0300

    Inline styles

--- src/Header.js
+++ src/Header.js
@@ -7,13 +7,18 @@ export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
 
   return(
-    <>
+    <div
+      style={{
+        background: "#CCC",
+        fontFamily: 'sans-serif',
+      }}
+    >
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>
         Mudar tema
       </Button>
       {props.children}
-    </>
+    </div>
   )
 }
 
