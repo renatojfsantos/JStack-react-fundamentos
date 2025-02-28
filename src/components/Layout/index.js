52139beb22d9eed3f9b85c6b9eb944569ec4a7cc
Renato Santos <renato_jfs@hotmail.com>
2025-02-28 00:56:25 -0300

    Exact prop and Link component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
+
 import Header from '../Header';
 import Footer from '../Footer';
 
 import Routes from '../../Routes';
 
+import { Nav } from './styles';
+
 export default function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+      </Nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
