35347fc7c89542467c82effb2ed62e7decb2e49f
Renato Santos <renato_jfs@hotmail.com>
2025-03-03 01:03:55 -0300

    Programmatic navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Container } from './styles';
 import { useTheme } from '../../contexts/ThemeContext';
+import { useHistory } from 'react-router-dom';
 
 export default function Header() {
   const { theme, handleToggleTheme } = useTheme();
+  const history = useHistory();
+  console.log("🚀 ~ Header ~ history:", history)
+
+  const handleNavigate = () => {
+    history.push('/');
+  }
 
   return (
     <Container>
       <h1>JStack's Blog</h1>
+      <button type="button" onClick={handleNavigate} style={{ color: '#FFF' }}>
+        Navegar
+      </button>
       <button type="button" onClick={handleToggleTheme}>
         {theme === 'dark' ? '🌞' : '🌜'}
       </button>
--- src/pages/PostDetail/index.js
+++ src/pages/PostDetail/index.js
@@ -1,20 +1,28 @@
 import React, { useMemo } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useHistory } from 'react-router-dom';
 
 export default function PostDetail() {
+  const history = useHistory();
   const params = useParams();
   const location = useLocation();
   const { search } = location;
   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
 
-  console.log("🚀 ~ params:", params)
-  console.log("🚀 ~ location:", location)
-  console.log("🚀 ~ search:", search)
-  console.log("🚀 ~ queryParams:", queryParams)
-  console.log("🚀 ~ queryParams.get('page'):", queryParams.get('page'))
+  // console.log("🚀 ~ params:", params)
+  // console.log("🚀 ~ location:", location)
+  // console.log("🚀 ~ search:", search)
+  // console.log("🚀 ~ queryParams:", queryParams)
+  // console.log("🚀 ~ queryParams.get('page'):", queryParams.get('page'))
+  const handleNavigate = () => {
+    history.push('/posts');
+  }
 
   return (
     <>
+      <button onClick={handleNavigate}>
+        Voltar para lista de posts
+      </button>
+
       <h1>Detalhe do Post</h1>
       <p>Id do post: {params.id}</p>
       <p>Autor: {params.author}</p>
